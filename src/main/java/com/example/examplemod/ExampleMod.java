ab37a4868e8a42c7e0fed01da65afa0ff4d97973
lilyfaerose <81891373+lilyfaerose@users.noreply.github.com>
2021-04-05 14:53:00 -0600

    Update ExampleMod.java
    final focus pass

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -19,9 +19,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("elemancy")
+@Mod(MODID)
 public class ExampleMod
 {
+
+public static final STRING MODID = "elemancy";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
